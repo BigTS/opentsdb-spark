f34868644b0b9b3bd3c5d6aa7feecb37c0069909
Ahmed Mahran <ahmed.mahran@badrit.com>
2016-03-12 16:54:59 +0200

    Make separate test cases for both old and new APIs

--- src/test/scala/org/bigts/rdd/opentsdb/TestSuite.scala
+++ src/test/scala/org/bigts/rdd/opentsdb/TestSuite.scala
@@ -4,31 +4,64 @@ import org.apache.spark.rdd.RDD
 import org.scalatest.{ShouldMatchers, FunSuite}
 
 class TestSuite extends FunSuite with ShouldMatchers {
-  test("Main") {
+  test("Old API") {
     val sparkMaster = "local"
     val zookeeperQuorum = "localhost"
     val zookeeperClientPort = "2181"
     val metric = "product.sales"
     val tagVal = "id->1"
-    val startD = "2016/01/01 10:00"
+    val startD = "2016/01/01 00:00"
     val endD = "2016/02/29 10:00"
 
-    val sc = CustomSparkContext.create(sparkMaster = sparkMaster, zookeeperQuorum = zookeeperQuorum,
+    val sc = CustomSparkContext.create(
+      sparkMaster = sparkMaster,
+      zookeeperQuorum = zookeeperQuorum,
       zookeeperClientPort = zookeeperClientPort)
 
     //Connection to OpenTSDB
     val sparkTSDB = new SparkTSDBQuery(zookeeperQuorum, zookeeperClientPort)
 
     //Create RDD from OpenTSDB
-    val dateOldAPI: RDD[(Long, Float)] =
-      sparkTSDB.generateRDD(metricName = metric, tagsKeysValues = tagVal, startdate = startD, enddate = endD, sc)
+    val dataOldAPI: RDD[(Long, Float)] = sparkTSDB.generateRDD(
+      metricName = metric,
+      tagsKeysValues = tagVal,
+      startdate = startD,
+      enddate = endD,
+      sc)
+
+    printRDD(dataOldAPI, "Old API")
+
+    dataOldAPI.count() should be (60)
+
+    sc.stop
+  }
+
+  test("New API") {
+    val sparkMaster = "local"
+    val zookeeperQuorum = "localhost"
+    val zookeeperClientPort = "2181"
+    val metric = "product.sales"
+    val tagVal = "id->1"
+    val startD = "2016/01/01 00:00"
+    val endD = "2016/02/29 10:00"
+
+    val sc = CustomSparkContext.create(
+      sparkMaster = sparkMaster,
+      zookeeperQuorum = zookeeperQuorum,
+      zookeeperClientPort = zookeeperClientPort)
 
     val hbaseSpark = new HBaseSparkTSDBQuery(zookeeperQuorum, zookeeperClientPort)
-    val dataHBaseSpark: RDD[(Long, Float)] =
-      hbaseSpark.generateRDD(metricName = metric, tagsKeysValues = tagVal, startdate = startD, enddate = endD, sc)
 
-    printRDD(dateOldAPI, "Old API")
+    val dataHBaseSpark: RDD[(Long, Float)] = hbaseSpark.generateRDD(
+      metricName = metric,
+      tagsKeysValues = tagVal,
+      startdate = startD,
+      enddate = endD,
+      sc)
+
     printRDD(dataHBaseSpark, "New API")
+
+    dataHBaseSpark.count() should be (60)
 
     sc.stop
   }
